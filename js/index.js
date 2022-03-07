bf054dc9cf991ecbeea8e8a1afc82dfa9c7d3090
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-07 18:28:19 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,47 @@
 // global variable
 const takeNoteField = document.getElementById('note-describe')
+const showAllNote = document.getElementById('result')
+const message = document.getElementById('message')
+// display All note
+const displayAllNote = () => {
+  const data = getNote()
+  console.log()
+  if (Object.keys(data).length !== 0) {
+    message.innerHTML = `
+    <div id="message">
+    <h2 class="text-center my-5 pr-3">Your Note</h2>
+    </div>
+      `
+    showAllNote.textContent = ''
+    let i = 1
+    for (const note in data) {
+      const div = document.createElement('div')
+      div.classList.add('col-md-3')
+      div.classList.add('col-sm-6')
+      div.innerHTML = `
+        <label for="note-describe" class="form-label text-secondary"
+                  >Note:${i++}</label
+                >
+                <textarea
+                  class="form-control"
+                  id="note-describe"
+                  disabled
+                  placeholder="Please Provide a Title And Than Describe"
+                  rows="4"
+                  cols="10"
+                >${data[note]}</textarea>
+        `
+      showAllNote.appendChild(div)
+    }
+  } else {
+    showAllNote.textContent = ''
+    message.innerHTML = `
+    <div id="message">
+    <h2 class="text-center my-5 pr-3">${'No Note Available'}</h2>
+    </div>
+      `
+  }
+}
 
 // display or hide function
 const displayOrHide = (id, className, existName) => {
@@ -15,15 +57,18 @@ const showNoteInput = (name, zihad, kowser) => {
 // submit note
 const submitNote = () => {
   const note = takeNoteField.value
-  pushToLocalStorage(note)
-  displayOrHide('show-note', 'd-none', 'd-block')
+  if (note) {
+    pushToLocalStorage(note)
+    displayOrHide('show-note', 'd-none', 'd-block')
+    displayAllNote()
+    takeNoteField.value = ''
+  }
 }
 
 // push to local storage
 const pushToLocalStorage = (note) => {
   const id = 'id' + Math.random().toString(16).slice(2)
   const data = getNote()
-  console.log(data)
   data[id] = note
   const dataStringfy = JSON.stringify(data)
   localStorage.setItem('note', dataStringfy)
@@ -32,7 +77,21 @@ const pushToLocalStorage = (note) => {
 // get note from local store
 const getNote = () => {
   const allNote = localStorage.getItem('note')
-  console.log(allNote)
-  let noteObj = JSON.parse(allNote)
+  let noteObj
+  if (allNote) {
+    noteObj = JSON.parse(allNote)
+  } else {
+    noteObj = {}
+  }
   return noteObj
 }
+
+// clear all note
+const clearAllNote = () => {
+  localStorage.removeItem('note')
+  displayAllNote()
+
+  console.log('clear')
+}
+
+displayAllNote()
