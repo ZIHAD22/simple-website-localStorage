9d1f7684c7125d53cfd26fe10f63421449d444e8
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-09 16:12:33 +0600

    session

--- js/guestIndex.js
+++ js/guestIndex.js
@@ -59,7 +59,7 @@ const displayAllNote = async () => {
     }
   } else {
     location.assign(
-      'https://zihad22.github.io/simple-website-localStorage/guestIndex.html',
+      'https://zihad22.github.io/simple-website-localStorage/login.html',
     )
   }
 }
